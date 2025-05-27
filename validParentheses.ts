89514d2eb7ab052b5ed2af709d4d20204a0d1ec6
s1f102100704 <s1f102100704@iniad.org>
2025-05-27 14:29:50 +0900

    難しすぎる

---
+++ validParentheses.ts
@@ -0,0 +1,10 @@
+const validParentheses = (str: string) => {
+  const stack: string[] = [];
+  const map = new Map<string, string>([
+    [")", "("],
+    ["}", "{"],
+    ["]", "["],
+  ]);
+  for (const s of str) {
+  }
+};
