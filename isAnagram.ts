173f69b0e95678a35af495a3bdb3bdafaf0e5448
s1f102100704 <s1f102100704@iniad.org>
2025-05-27 14:06:36 +0900

    isAnagram

---
+++ isAnagram.ts
@@ -0,0 +1,30 @@
+const isAnagram = (s: string, t: string) => {
+  const sMap = new Map<string, number>();
+  const tMap = new Map<string, number>();
+
+  const setStr = (word: string, map: Map<string, number>) => {
+    for (const str of word) {
+      if (map.has(str)) {
+        map.set(str, map.get(str)! + 1);
+      } else {
+        map.set(str, 1);
+      }
+    }
+  };
+
+  setStr(s, sMap);
+  setStr(t, tMap);
+
+  if (sMap.size !== tMap.size) return false;
+
+  for (const [char, count] of sMap) {
+    if (!tMap.has(char) || count !== tMap.get(char)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isAnagram("anagram", "nagaram")); // true
+console.log(isAnagram("rat", "car")); // false
+console.log(isAnagram("aacc", "ccac"));
