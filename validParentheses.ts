004b39e2f49f39f6ba7b202996674dc0bd04299c
s1f102100704 <s1f102100704@iniad.org>
2025-05-28 13:37:47 +0900

    validParentheses

--- validParentheses.ts
+++ validParentheses.ts
@@ -6,5 +6,22 @@ const validParentheses = (str: string) => {
     ["]", "["],
   ]);
   for (const s of str) {
+    if (s == "(" || s == "{" || s == "[") {
+      stack.push(s);
+    } else {
+      const top = stack.pop();
+      if (top !== map.get(s)) {
+        return false;
+      }
+    }
   }
+  if (stack.length == 0) {
+    return true;
+  }
+  return false;
 };
+console.log(validParentheses("()"));
+console.log(validParentheses("()[]{}"));
+console.log(validParentheses("(]"));
+console.log(validParentheses("([)]"));
+console.log(validParentheses("{[]}"));
